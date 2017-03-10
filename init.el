6f3ae1801baca285a6391e08a478d27415b3325a
Simon Gregersen <simonsog@gmail.com>
2017-03-10 16:44:57 +0100

    delete-current-buffer-file

--- init.el
+++ init.el
@@ -211,8 +211,23 @@
      '(defadvice ,mode (after rename-modeline activate)
         (setq mode-name ,new-name))))
 
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
 ;; custom key bindings
 (global-set-key (kbd "M-j")
                 (lambda ()
                   (interactive)
                   (join-line -1)))
+
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
